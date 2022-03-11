70c2b40045a7b233b3415c564fe56adac4cf3c59
Ruben Lopes <ru4en@pm.me>
2022-03-11 19:58:00 +0000

    Added print stack function tryed to implement bubble sort but failed

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "cardlib.h"
+#include <iostream>
+
+int main() {
+    
+    return 0;
+}
--- src/sortcard.cpp
+++ src/sortcard.cpp
@@ -7,16 +7,58 @@
 #include <iostream>
 using namespace std;
 
-const int maxCard = 3;
+const int maxCard = 7;
 
 aCard thePack[maxCard];
+
+aCard* bubbleSort(aCard c[])
+{
+    aCard x;
+    bool sorted=false;
+    while (sorted!=true) {
+        for (int i = 0; i < maxCard; i++)
+        {
+            if (c[i].cardSuit > c[i + 1].cardSuit)
+            {
+                x = c[i + 1];
+                c[i + 1] = c[i];
+                c[i] = x;
+                
+            }else
+            {
+                sorted=true;
+            }
+        } 
+    }
+    return c;
+}
+
+void printPack(aCard c[])
+{
+    string suteNames[] = {"Hearts", "Clubs", "Diamonds", "Spades"};
+    for (int i=0; i < maxCard; i++)
+    {
+        cout << c[i].cardVal << " of ";
+        cout << suteNames[c[i].cardSuit] << endl;
+    }
+}
 
 int main()
 {
     cout << "Card Sorting!\n";
 
     for (int ct = 0; ct < maxCard; ct++)
-        thePack[ct] = getCard("14398132");   // change to your student number
+    {
+        thePack[ct] = getCard("30021591");   // change to your student number
+    }
+    cout << "\nYour card are: " << endl;
+    printPack(thePack);
+    aCard* sortedCards;
+    sortedCards = bubbleSort(thePack);
+    cout << "\nSorted card are: " << endl;
+    printPack(sortedCards);
 }
 
+// # Questions
 
+// How do I covert "thePack[ct].cardSuit" to Sute names?
