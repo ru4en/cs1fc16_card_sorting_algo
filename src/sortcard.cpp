75e65da0180f5150afe8cda1c99beb4097d6aa1f
Ruben Lopes <ru4en@pm.me>
2022-03-16 19:38:28 +0000

    Added function to check if order is correct, quickSort also added but lacks use of correct order function and conter

--- src/sortcard.cpp
+++ src/sortcard.cpp
@@ -1,5 +1,4 @@
 // SortCard.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include "cardlib.h"
 
@@ -11,39 +10,93 @@ const int maxCard = 20;
 
 aCard thePack[maxCard];
 
-aCard* bubbleSort(aCard c[])
-{
+int bubbleSortCounter = 0;
+int quickSortCounter = 0;
+
+bool corOrder(aCard a, aCard b, bool val) {
+    if (val)
+    {
+        if (a.cardVal < b.cardVal)
+        {
+            return 1;
+        }else
+        {
+            return 0;
+        }
+    }else
+    {
+        if (a.cardSuit < b.cardSuit)
+        {
+            return 1;
+        }else
+        {
+            return 0;
+        }
+    }
+    return -1;
+}
+
+aCard* bubbleSort(aCard c[], bool val)
+{       
     aCard x;
 
-    for (int j = 0; j < maxCard; j++)
+    for (int j = 0; j < (maxCard-1); j++)
     {
-        for (int i = 0; i < maxCard; i++)
+        for (int i = 0; i < (maxCard-1); i++)
         {
-            if (c[i].cardVal < c[i + 1].cardVal)
+            if (corOrder(c[i], c[i+1], val) == 1)
             {
                 x = c[i + 1];
                 c[i + 1] = c[i];
                 c[i] = x;
             }
+            bubbleSortCounter++;
         }
     }    
     return c;
 }
 
-aCard* quickSort(aCard c[], int start = 0, int end = maxCard)
+int partition(aCard c[], int left, int right)
 {
+    int p = left - right;
+    int store;
     aCard x;
+    quickSortCounter++;
 
-    if (start >= end)
+    for (int i = 0; i < (p); i++)
     {
-        
+        if (c[i].cardVal <= c[right].cardVal)
+        {
+            x = c[left];
+            c[left] = c[i];
+            c[i] = x;
+        }
     }
+
+    x = c[left];
+    c[left] = c[right];
+    c[right] = x;
+
+    return left;
+}
+
+aCard* quickSort(aCard c[], int left, int right)
+{
+    int piv;
+
+    if (left > right)
+    {
+        piv = partition(c, left, right);
+        quickSort(c, left, (piv-1));
+        quickSort(c, left, (piv-1));
+    }
+    return c;
 }
 
 void printPack(aCard c[])
 {
     string suteNames[] = {"♥️ Hearts", "♣️ Clubs", "♦️ Diamonds", "♠️ Spades"};
-    string cardValue[] = {"NULL", "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+    string cardValue[] = {"-", "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
     for (int i=0; i < maxCard; i++)
     {
         cout << cardValue[c[i].cardVal] << " of " << suteNames[c[i].cardSuit] << endl;
@@ -58,14 +111,20 @@ int main()
     {
         thePack[ct] = getCard("30021591");   // change to your student number
     }
+
     cout << "\nYour card are: " << endl;
     printPack(thePack);
-    aCard* sortedCards;
-    sortedCards = bubbleSort(thePack);
-    cout << "\nSorted card are: " << endl;
-    printPack(sortedCards);
+
+    aCard* bubbleSortedCards;
+    bubbleSortedCards = bubbleSort(thePack, 1);
+    bubbleSortedCards = bubbleSort(bubbleSortedCards, 0);
+    cout << "\nBubble Sorted card are: " << endl;
+    printPack(bubbleSortedCards);
+    cout << bubbleSortCounter << " moves for " << maxCard << " cards." << endl;
+
+    aCard* quickSortedCards;
+    quickSortedCards = quickSort(thePack, 0, maxCard-1);
+    cout << "\nQuick Sorted card are: " << endl;
+    printPack(quickSortedCards);
+    cout << quickSortCounter << " moves for " << maxCard << " cards." << endl;
 }
-
-// # Questions
-
-// How do I covert "thePack[ct].cardSuit" to Sute names?
